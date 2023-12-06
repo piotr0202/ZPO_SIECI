7e9c4ec7966bdcced8c2977266a9d50d18612012
Paweł Kocur <pawel.kocur282003@gmail.com>
2023-12-06 20:01:53 +0100

    próba z main

--- storage_types.hpp
+++ storage_types.hpp
@@ -1,1 +1,21 @@
-#include 
+#ifndef STORAGE_TYPES_HPP
+#define STORAGE_TYPES_HPP
+
+#include <list>
+
+#include "package.hpp"
+
+enum class PackageQueueType {
+    FIFO,
+    LIFO
+};
+
+class IPackageStockpile {
+public:
+    // Tworzenie const interatora IPackageStockpile::const_iterator
+    using const_iterator = std::list<Package>::const_iterator;
+
+
+}
+
+#endif
