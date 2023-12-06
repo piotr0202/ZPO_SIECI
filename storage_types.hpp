a09d98a5447f7098fa1db5525edc978353b861fa
Paweł Kocur <pawel.kocur282003@gmail.com>
2023-12-06 20:07:11 +0100

    aktualizacja

--- storage_types.hpp
+++ storage_types.hpp
@@ -15,6 +15,21 @@ public:
     // Tworzenie const interatora IPackageStockpile::const_iterator
     using const_iterator = std::list<Package>::const_iterator;
 
+    virtual void push(Package&& package) = 0;
+
+    virtual std::size_t size() const = 0;
+
+    virtual bool empty() const = 0;
+
+    virtual const_iterator cbegin() const = 0;
+
+    virtual const_iterator cend() const = 0;
+
+    virtual const_iterator begin() const = 0;
+
+    virtual const_iterator end() const = 0;
+
+    virtual ~IPackageStockpile() = default;
 
 }
 
